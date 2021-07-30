5bfc10753371e80e9e73542a0c5eea07afa75f1b
Shallon Kobusinge <kobshallon041@gmail.com>
2021-07-31 00:13:52 +0200

    fix: apphighlight color value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color = ''
   title = 'angular-directives';
+  
+ 
 }
--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -1,20 +1,20 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  @Input() appHighlight = '';
 
-  constructor(public el: ElementRef) { }
-  @HostListener('mouseenter') onMouseEnter(){
-    this.highlight('yellow')
+  constructor(public el: ElementRef) {}
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.appHighlight);
   }
-  @HostListener('mouseleave') onMouseLeave(){
-    this.highlight('')
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
   }
-  private highlight(color: string){
-    this.el.nativeElement.style.backgroundColor = color
-
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+    console.log(color);
   }
-
 }
