9b4631cc2dadd2feb9efc4b2dd9440d0ee9d3b5b
Shallon Kobusinge <kobshallon041@gmail.com>
2021-07-31 00:19:47 +0200

    feat: adding default attribute directive

--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -5,10 +5,11 @@ import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 })
 export class HighlightDirective {
   @Input() appHighlight = '';
+  @Input() defaultColor = ''
 
   constructor(public el: ElementRef) {}
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight(this.appHighlight);
+    this.highlight(this.appHighlight || this.defaultColor || 'red');
   }
   @HostListener('mouseleave') onMouseLeave() {
     this.highlight('');
